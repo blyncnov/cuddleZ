8b9492664f352600a60b201523599c04fe652bb2
blyncnov <bholuwatife00@gmail.com>
2021-07-15 12:38:06 +0100

    fourt commit

--- components/desc.jsx
+++ components/desc.jsx
@@ -9,6 +9,8 @@ const Description = () => {
                     <h1 className="desc__desc">Cuddle-Z pioneering virtual care model is built around women and families, delivering better outcomes and lower costs for everyone.</h1>
                     <p style={{ textAlign: "center" }}>Marvel, and Sketch and accelerate your process from idea to test.</p>
                     <div className="desc__boxes">
+
+
                         <div className="desc__box">
                             <div className="box__title">
                                 <h4> <FaEllo /> Whole-person care</h4>
@@ -19,6 +21,8 @@ const Description = () => {
                                 </p>
                             </div>
                         </div>
+
+
                         <div className="desc__box active">
                             <div className="box__title">
                                 <h4><FaGetPocket /> Exceptional outcomes</h4>
@@ -29,6 +33,37 @@ const Description = () => {
                                 </p>
                             </div>
                         </div>
+
+
+                        <div className="desc__box">
+                            <div className="box__title">
+                                <h4> <FaEllo /> Whole-person care</h4>
+                            </div>
+                            <br />
+                            <div className="box__title">
+                                <p> A holistic approach that offers comprehensive care for all families.
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    {/* testimg here */}
+
+                    <div className="desc__boxes rm">
+                        <div className="desc__box active">
+                            <div className="box__title">
+                                <h4><FaGetPocket /> Exceptional outcomes</h4>
+                            </div>
+                            <br />
+                            <div className="box__title">
+                                <p> Dedicated Care Advocates and data-driven insights deliver better outcomes.
+                                </p>
+                            </div>
+                        </div>
+
+
                         <div className="desc__box">
                             <div className="box__title">
                                 <h4><FaLess /> Lower costs</h4>
